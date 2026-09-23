35c0fb434fefdfb8068bfb71e8d2362bffe30536
Davy <davylucasdelimatrindade@gmail.com>
2026-09-23 14:53:58 -0300

    Chore: Estrutura inicial Carros

---
+++ Aula01/Models/Carro.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula01.Models
+{
+    public class Carro
+    {
+
+        //PROPRIEDADES(ATRIBUTOS/CARACTERISTICAS)
+        
+        public string Marca { get; set; }
+        
+        public string Modelo { get; set; }
+
+        public string Cor {  get; set; }
+
+        //MÉTODOS (AÇÕES)
+
+        public string Acelerar()
+        {
+            return $"O {Modelo} {Marca} está acelerando"
+        }
+    }
+}
